ea6d2ec7ff6007caa410d75ca40fc010597dac0f
Homitz <side9207@gmail.com>
2024-03-07 16:57:02 +0800

    init2

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.guess_number
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import android.view.ActionMode
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,9 +14,19 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG:String = MainActivity::class.java.simpleName
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
+
+        handler.postDelayed({
+            Toast.makeText(this, "5秒後的操作就行了!",Toast.LENGTH_SHORT).show()
+        },5000)
+
+
+
 
         val textView = findViewById<TextView>(R.id.EditText)
         val result_testView = findViewById<TextView>(R.id.result_text)
@@ -25,12 +40,11 @@ class MainActivity : AppCompatActivity() {
         guess_button.setOnClickListener{
             textView.text=editText.text
             validate_num=editText.text.toString().toInt()-secret
-            var ans_str:String="你猜對了喔"
-
+            var ans_str:String="Congrets!"
             if (validate_num>0){
-                ans_str="你猜太大了喔"
+                ans_str="You r so big"
             }else if (validate_num<0){
-                ans_str="你猜太小了喔"
+                ans_str="You guess too small"
             }
             textView.text=ans_str
             //Toast.makeText(this, editText.text,Toast.LENGTH_LONG).show()
@@ -38,7 +52,10 @@ class MainActivity : AppCompatActivity() {
         }
         reset_button.setOnClickListener{
             validate_num= Random().nextInt(10)+1
-            textView.text="讓我們在猜一次"
-        }
+            textView.text="Try again"
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
