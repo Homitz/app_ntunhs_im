3db6c01ad841c22326d00b7475ccb4530d73e945
Homitz <side9207@gmail.com>
2024-04-18 16:22:40 +0800

    20240418

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -1,31 +1,32 @@
 package com.example.guess_number
 
 import android.nfc.Tag
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.PersistableBundle
 import android.view.ActionMode
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.guess_number.databinding.ActivityMainBinding
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
-    val TAG:String = MainActivity::class.java.simpleName
+    val TAG: String = MainActivity::class.java.simpleName
     private lateinit var handler: Handler
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         handler = Handler(Looper.getMainLooper())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         handler.postDelayed({
-            Toast.makeText(this, "5秒後的操作就行了!",Toast.LENGTH_SHORT).show()
-        },5000)
-
-
+            Toast.makeText(this, "5秒後的操作就行了!", Toast.LENGTH_SHORT).show()
+        }, 5000)
 
 
         val textView = findViewById<TextView>(R.id.EditText)
@@ -34,28 +35,30 @@ class MainActivity : AppCompatActivity() {
         val reset_button = findViewById<Button>(R.id.Button2)
         val editText = findViewById<EditText>(R.id.EditText)
 
-        var validate_num:Int
-        val secret : Int = Random().nextInt(10) +1
+        var validate_num: Int
+        val secret: Int = Random().nextInt(10) + 1
 
-        guess_button.setOnClickListener{
-            textView.text=editText.text
-            validate_num=editText.text.toString().toInt()-secret
-            var ans_str:String="Congrets!"
-            if (validate_num>0){
-                ans_str="You r so big"
-            }else if (validate_num<0){
-                ans_str="You guess too small"
+        guess_button.setOnClickListener {
+            binding.EditText.text = editText.text
+            validate_num = editText.text.toString().toInt() - secret
+            var ans_str: String = "Congrets!"
+            if (validate_num > 0) {
+                ans_str = "You r so big"
+            } else if (validate_num < 0) {
+                ans_str = "You guess too small"
             }
-            textView.text=ans_str
+            textView.text = ans_str
             //Toast.makeText(this, editText.text,Toast.LENGTH_LONG).show()
             //AlterDialog.Builder(this).setTitle("onclick").setMessage("onclick").create().show()
         }
-        reset_button.setOnClickListener{
-            validate_num= Random().nextInt(10)+1
-            textView.text="Try again"
+
+        binding.Button2.setOnClickListener {
+            validate_num = Random().nextInt(10) + 1
+            textView.text = "Try again"
+        }
     }
-    override fun onDestroy() {
-        super.onDestroy()
-        handler.removeCallbacksAndMessages(null)
+        override fun onDestroy() {
+            super.onDestroy()
+            handler.removeCallbacksAndMessages(null)
+        }
     }
-}
